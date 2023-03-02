3dab46d9a1409acc6e34d2c00cd0c9aa3e88eb16
Павел <melnicpavelvasile1983@gmail.com>
2023-03-02 19:02:44 +0200

    homew

---
+++ Power.java
@@ -0,0 +1,19 @@
+package HomeWork;
+
+public class Power {
+    public static void main(String[] args) {
+
+        int e;
+        int result;
+
+        for (int i = 0; i < 10; i++) {
+            result = 1;
+
+
+            System.out.println( "result" + 'i' );
+
+        }
+
+    }
+}
+
