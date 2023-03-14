26b8e255b6a89156e6002b3cfeeafb1d3488f4a3
Павел <melnicpavelvasile1983@gmail.com>
2023-03-14 16:50:37 +0200

    update

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,12 +1,21 @@
 package HomeWork;
 
 import java.util.Scanner;
-
-public class EvenOdd {
+class EvenOdd {
     public static void main(String[] args) {
-        Scanner num = new Scanner(System.in);
-        System.out.println("Введите число:");
-        String x = num.nextDouble()%2==0 ? "Четное" : "Не четное";
-        System.out.println(x);
+        for (; ; ) {
+            double num;
+            Scanner n = new Scanner(System.in);
+            System.out.print("\nEnter integer number: ");
+            num = n.nextDouble();
+
+            if ((num % 2) == 0) {
+                System.out.print("Number " + num + " is EVEN");
+            } else {
+                System.out.print("Number " + num + " is ODD");
+            }
+
+            System.out.println();
+        }
     }
 }
