372e109940ffa4eaa75804a575ad8859cdcdfffe
Павел <melnicpavelvasile1983@gmail.com>
2023-04-05 20:22:20 +0300

    update

--- Help.java
+++ Help.java
@@ -1,59 +1,35 @@
 package HomeWork;
-class Help {
-    void help(int what) {
-        switch (what) {
+
+public class Help {
+    public static void main(String[] args) throws java.io.IOException {
+        char choice;
+
+        System.out.println("Справка");
+        System.out.println(" 1. if");
+        System.out.println(" 2. switch");
+        System.out.println("Выбберите: ");
+        choice = (char) System.in.read();
+
+        System.out.println("\n");
+
+        switch (choice) {
             case '1' -> {
-                System.out.println("Onepaтop if:\n");
-                System.out.println("if(ycлoвиe) оператор;");
-                System.out.println("else оператор;");
+                System.out.println("Оператор if:\n");
+                System.out.println("if(условие) оператор;");
+                System.out.println("else оператор");
             }
             case '2' -> {
-                System.out.println("Oпepaтop switch:\n");
-                System.out.println("switch(выpaжeниe) {");
+                System.out.println("Оператор switch:\n");
+                System.out.println("switch (выражение) {");
                 System.out.println(" case константа:");
                 System.out.println(" последовательность операторов");
                 System.out.println(" break;");
-                System.out.println(" // ...");
+                System.out.println("// ...");
                 System.out.println("}");
             }
-            case '3' -> {
-                System.out.println("Onepaтop for:\n");
-                System.out.print("for(инициaлизaция; условие; итерация)");
-                System.out.println(" оператор;");
-            }
-            case '4' -> {
-                System.out.println("Onepaтop while:\n");
-                System.out.println("while(ycлoвиe) оператор;");
-            }
-            case '5' -> {
-                System.out.println("Oпepaтop do-while:\n");
-                System.out.println("do {");
-                System.out.println(" оператор;");
-                System.out.println("} while (условие;");
-            }
-            case '6' -> {
-                System.out.println("Oпepaтop break:\n");
-                System.out.println("break; или break метка;");
-            }
-            case '7' -> {
-                System.out.println("Oпepaтop continue:\n");
-                System.out.println("continue; или continue метка;");
-            }
+            default -> System.out.println("Запрос не найден");
         }
-        System.out.println();
-    }
-    void showmenu() {
-        System.out.println("Cпpaвкa:");
-        System.out.println(" 1. if");
-        System.out.println(" 2. switch");
-        System.out.println(" 3. for");
-        System.out.println(" 4. while");
-        System.out.println(" 5. do-while");
-        System.out.println(" 6. break");
-        System.out.println(" 7. continue\n");
-        System.out.print("Bыбepитe (q - выход): ");
-    }
-    boolean isvalid (int ch) {
-        return !(ch < '1' | ch > '7' & ch != 'q');
+
+
     }
 }
