f8b8eb5604aeaa1a0815cf7794de1244b8b021aa
Павел <melnicpavelvasile1983@gmail.com>
2023-04-13 13:15:22 +0300

    update

--- Help.java
+++ Help.java
@@ -2,13 +2,23 @@ package HomeWork;
 
 public class Help {
     public static void main(String[] args) throws java.io.IOException {
-        char choice;
+        char choice, ignore;
 
-        System.out.println("Справка");
-        System.out.println(" 1. if");
-        System.out.println(" 2. switch");
-        System.out.println("Выбберите: ");
+        do {
+            System.out.println("Справка");
+            System.out.println(" 1. if");
+            System.out.println(" 2. switch");
+            System.out.println(" 3. for");
+            System.out.println(" 4. while");
+            System.out.println(" 5. do-while\n");
+            System.out.println("Выбберите: ");
+
         choice = (char) System.in.read();
+
+        do {
+            ignore = (char) System.in.read();
+        } while (ignore != '\n');
+        } while ( choice < '1' | choice > '5');
 
         System.out.println("\n");
 
@@ -27,9 +37,21 @@ public class Help {
                 System.out.println("// ...");
                 System.out.println("}");
             }
-            default -> System.out.println("Запрос не найден");
+            case '3' -> {
+                System.out.println("Оператор for:\n");
+                System.out.println("for(инициализация; условие; итерация)");
+                System.out.println(" оператор;");
+            }
+            case '4' -> {
+                System.out.println("Оператор while\n");
+                System.out.println("while(условие) оператор;");
+            }
+            case '5' -> {
+                System.out.println("Оператор do-hile:\n");
+                System.out.println("do {");
+                System.out.println(" оператор;");
+                System.out.println("} while (условие);");
+            }
         }
-
-
     }
 }
