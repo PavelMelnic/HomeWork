257a1d31f5e21dce0a257361aceb9c96bfd67439
Павел <melnicpavelvasile1983@gmail.com>
2023-04-25 12:05:43 +0300

    update

--- L7HW_2.java
+++ L7HW_2.java
@@ -2,7 +2,13 @@ package HomeWork;
 
 public class L7HW_2 {
     public static void main(String[] args) {
-        int[] arr = new int[]{21, 4, -6, -8, 110, 52, 44, 126, 181, -20};
+
+        int arr[] = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = ((int)(Math.random() * 10)-3);
+            System.out.println(arr[i]);
+        }
+
         double avg = 0;
         double sum = 0;
         double mod = 0;
